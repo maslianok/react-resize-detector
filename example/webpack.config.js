e6bdfe138c2527a4570e227706a0ad4fb29e730c
maslianok <maslianok@gmail.com>
2016-03-13 16:49:15 +0200

    Webpack config

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,33 +1,36 @@
-var path = require('path')
-var webpack = require('webpack')
+/* eslint no-var: 0 */
+var path = require('path');
+var webpack = require('webpack');
+
+var entries = ['./index'];
+if (process.NODE_ENV !== 'production') {
+  entries.unshift('webpack-hot-middleware/client');
+}
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
-  entry: [
-    'webpack-hot-middleware/client',
-    './index'
-  ],
+  entry: entries,
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/dist/',
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
   ],
-  resolve: { 
-    alias: {'react-resize-detector': path.join(__dirname, '..', 'lib', 'index.js')},
-    root: path.join(__dirname, 'node_modules')
+  resolve: {
+    alias: { 'react-resize-detector': path.join(__dirname, '..', 'lib', 'index.js') },
+    root: path.join(__dirname, 'node_modules'),
   },
   module: {
     loaders: [
       {
         test: /\.js$/,
-        loaders: [ 'babel' ],
+        loaders: ['babel'],
         exclude: /node_modules/,
-        include: __dirname
-      }
-    ]
-  }
-}
+        include: __dirname,
+      },
+    ],
+  },
+};
