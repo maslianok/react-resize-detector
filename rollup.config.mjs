4c5adc6909109170e3b62ac6c98194dc8cadb5ec
maslianok <maslianok@gmail.com>
2023-01-18 21:09:41 +0100

    remove sourcemaps

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -8,7 +8,6 @@ import packageJson from './package.json' assert { type: 'json' };
 const getOutput = (path, format) => ({
   file: path,
   format: format,
-  sourcemap: true,
   compact: true,
   exports: 'named'
 });
