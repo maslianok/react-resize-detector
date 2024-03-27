788d95c718b2ebf553950d3de0c31a3b1737c367
Roman Zhuravlov <snelsithepirate@gmail.com>
2024-03-27 14:01:17 +0200

    Revert changes to eslint config

--- eslint.config.js
+++
@@ -1,5 +0,0 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import prettier from 'eslint-config-prettier';
-
-export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, prettier);
