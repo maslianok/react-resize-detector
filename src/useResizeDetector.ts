d3031f00c066b0863998ec03b9e220194453ee4d
Roman Zhuravlov <snelsithepirate@gmail.com>
2024-04-01 00:30:00 +0300

    Group related logic into custom hooks. Minor code cleanup and refactor

--- src/useResizeDetector.ts
+++ src/useResizeDetector.ts
@@ -1,9 +1,9 @@
-import { useEffect, useState, useRef, useCallback, useMemo } from 'react';
+import { useEffect, useState, useRef, useCallback } from 'react';
 import type { DebouncedFunc } from 'lodash';
 
-import { patchResizeCallback, useCallbackRef } from './utils';
+import { getDimensions, patchResizeCallback, useCallbackRef, useRefProxy } from './utils';
 
-import type { OnRefChangeType, Dimensions, UseResizeDetectorReturn, useResizeDetectorProps } from './types';
+import type { Dimensions, UseResizeDetectorReturn, useResizeDetectorProps } from './types';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function useResizeDetector<T extends HTMLElement = any>({
@@ -17,9 +17,10 @@ function useResizeDetector<T extends HTMLElement = any>({
   observerOptions,
   onResize
 }: useResizeDetectorProps<T> = {}): UseResizeDetectorReturn<T> {
+  // If `skipOnMount` is enabled, skip the first resize event
   const skipResize = useRef<boolean>(skipOnMount);
 
-  // Wrap the onResize callback with a ref to avoid re-renders
+  // Wrap the `onResize` callback with a ref to avoid re-renders
   const onResizeRef = useCallbackRef(onResize);
 
   const [size, setSize] = useState<Dimensions>({
@@ -27,73 +28,10 @@ function useResizeDetector<T extends HTMLElement = any>({
     height: undefined
   });
 
-  // we are going to use this ref to store the last element that was passed to the hook
-  const [refElement, setRefElement] = useState<T | null>(targetRef?.current || null);
+  // Create a proxy ref to handle conditional rendering and dynamic ref changes of the target element
+  const { refProxy, refElement } = useRefProxy(targetRef);
 
-  // if targetRef is passed, we need to update the refElement
-  // we have to use setTimeout because ref get assigned after the hook is called
-  // in the future releases we are going to remove targetRef and force users to use ref returned by the hook
-  if (targetRef) {
-    setTimeout(() => {
-      if (targetRef.current !== refElement) {
-        setRefElement(targetRef.current);
-      }
-    }, 0);
-  }
-
-  // this is a memo that will be called every time the ref is changed
-  // This proxy will properly call setState either when the ref is called as a function or when `.current` is set
-  // we call setState inside to trigger rerender
-  const refProxy: OnRefChangeType<T> = useMemo(
-    () =>
-      new Proxy(
-        node => {
-          if (node !== refElement) {
-            setRefElement(node);
-          }
-        },
-        {
-          get(target, prop) {
-            if (prop === 'current') {
-              return refElement;
-            }
-            return target[prop];
-          },
-          set(target, prop, value) {
-            if (prop === 'current') {
-              setRefElement(value);
-            } else {
-              target[prop] = value;
-            }
-            return true;
-          }
-        }
-      ),
-    [refElement]
-  );
-
-  // Only update the size if one of the observed dimensions has changed
-  const shouldSetSize = useCallback(
-    (prevSize: Dimensions, nextSize: Dimensions) => {
-      if (prevSize.width === nextSize.width && prevSize.height === nextSize.height) {
-        // skip if dimensions haven't changed
-        return false;
-      }
-
-      if (
-        (prevSize.width === nextSize.width && !handleHeight) ||
-        (prevSize.height === nextSize.height && !handleWidth)
-      ) {
-        // process `handleHeight/handleWidth` props
-        return false;
-      }
-
-      return true;
-    },
-    [handleWidth, handleHeight]
-  );
-
-  const box = observerOptions?.box;
+  const { box } = observerOptions || {};
 
   const resizeCallback: ResizeObserverCallback = useCallback(
     (entries: ResizeObserverEntry[]) => {
@@ -104,37 +42,12 @@ function useResizeDetector<T extends HTMLElement = any>({
         return;
       }
 
-      // Calculates the dimensions of the element based on the current box model
-      // Refs: https://developer.mozilla.org/en-US/docs/Learn/CSS/Building_blocks/The_box_model
-
-      // Value	          Border	  Padding	  Inner Content
-      // ---------------------------------------------------
-      // 'border-box'	    Yes	      Yes	      Yes
-      // 'content-box'	  No	      No	      Yes
-      //  undefined       No	      No?	      Yes
-      const getDimensions = (entry: ResizeObserverEntry) => {
-        if (box === 'border-box') {
-          return {
-            width: entry.borderBoxSize[0].inlineSize,
-            height: entry.borderBoxSize[0].blockSize
-          };
-        }
-
-        if (box === 'content-box') {
-          return {
-            width: entry.contentBoxSize[0].inlineSize,
-            height: entry.contentBoxSize[0].blockSize
-          };
-        }
-
-        return {
-          width: entry.contentRect.width,
-          height: entry.contentRect.height
-        };
-      };
+      // Only update the size if one of the observed dimensions has changed
+      const shouldSetSize = (prevSize: Dimensions, nextSize: Dimensions) =>
+        (handleWidth && prevSize.width !== nextSize.width) || (handleHeight && prevSize.height !== nextSize.height);
 
       entries.forEach(entry => {
-        const dimensions = getDimensions(entry);
+        const dimensions = getDimensions(entry, box);
         setSize(prevSize => {
           if (!shouldSetSize(prevSize, dimensions)) return prevSize;
           onResizeRef?.({
@@ -146,7 +59,7 @@ function useResizeDetector<T extends HTMLElement = any>({
         });
       });
     },
-    [handleWidth, handleHeight, skipResize, shouldSetSize, box]
+    [handleWidth, handleHeight, skipResize, box]
   );
 
   // Throttle/Debounce the resize event if refreshMode is configured
